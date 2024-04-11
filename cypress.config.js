5d379ed768cbcae1c7cc287f68f68f7f68e4300c
ilyas <ilyas.soufiane96@gmail.com>
2024-04-11 03:24:19 +0200

    add reporterOptions

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,12 @@ module.exports = defineConfig({
     charts: true,
     reportPageTitle: 'Report test | Youtrainers',
     embeddedScreenshots: true,
+    ignoreVideos: false,
+    videoOnFailOnly: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
+  video: true,
   e2e: {
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
